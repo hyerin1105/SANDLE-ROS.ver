e5454a273533c88f21366c6bd26758786e1a3c0f
hyerin1105 <hyerin328115@likelion.org>
2023-09-01 20:23:21 +0900

    sandle

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', main.views.main, name="main"),
     path('goods', main.views.goods, name="goods"),
     path('complete', main.views.complete, name="complete"),
+    path('end', main.views.end, name="end"),
 ]
--- main/views.py
+++ main/views.py
@@ -20,9 +20,12 @@ def goods(request):
     #response_data = {'message': 'ROS message sent successfully'}
     #return JsonResponse(response_data)
 
-rospy.init_node('ros_web_interface', anonymous=True)
 def complete(request):
-    pub = rospy.Publisher('button_press', Bool, queue_size=10)
-    msg = Bool(data=True)
-    pub.publish(msg)
     return render(request, 'complete.html')
+
+rospy.init_node('ros_web_interface', anonymous=True)
+def end(request):
+    pub2 = rospy.Publisher('button_press2', Bool, queue_size=10)
+    msg2 = Bool(data=True)
+    pub2.publish(msg2)
+    return render(request, 'end.html')
