b0032a44e73bc64cdd947efd66901ba5bae2ee46
hyerin1105 <hyerin328115@likelion.org>
2023-09-15 15:28:39 +0900

    sandle

--- main/views.py
+++ main/views.py
@@ -13,6 +13,9 @@ from django.contrib.auth.decorators import login_required
 
 ########### ceo ###########
 def wait(request):
+    pub2 = rospy.Publisher('button_go', Bool, queue_size=10)
+    msg2 = Bool(data=True)
+    pub2.publish(msg2)
     return render(request, 'wait.html')
 
 def loading(request):
@@ -23,42 +26,38 @@ def checking(request):
 
 ########### customer ###########
 def main1(request):
+    rospy.init_node('ros_web_interface', anonymous=True)
+
     pub = rospy.Publisher('button_open', Bool, queue_size=10)
     msg = Bool(data=True)
     pub.publish(msg)
     
-    rospy.init_node('ros_web_interface', anonymous=True)
     pub2 = rospy.Publisher('button_go', Bool, queue_size=10)
     msg2 = Bool(data=True)
     pub2.publish(msg2)
     return render(request,'main1.html')
 
-rospy.init_node('ros_web_interface', anonymous=True)
 @login_required(login_url='managment/login/')
 def goods(request):
-    pub = rospy.Publisher('button_open', Bool, queue_size=10)
-    msg = Bool(data=True)
-    pub.publish(msg)
     return render(request, 'goods.html')
-
-    #msg = Bool(data="Button Pressed")
-    #pub.publish(msg)
-    #response_data = {'message': 'ROS message sent successfully'}
-    #return JsonResponse(response_data)
 """
-@login_required(login_url='/login/')
 def goods(request, customer_id): #views.py의 pk변수명과 urls.py의 변수명은 같아야 함
     customer = get_object_or_404(Customer, pk=customer_id) #모델과 pk를 customer_id라고 부를거야
     customer_form = LoginForm()
     return render(request,'goods.html', {'customer' : customer}) #값을 보낼거임
-"""    
-
+"""
+rospy.init_node('ros_web_interface', anonymous=True)
 def complete(request):
+    rospy.init_node('ros_web_interface', anonymous=True)
+
+    pub = rospy.Publisher('button_open', Bool, queue_size=10)
+    msg = Bool(data=True)
+    pub.publish(msg)
     return render(request, 'complete.html')
 
 rospy.init_node('ros_web_interface', anonymous=True)
 def end(request):
-    pub2 = rospy.Publisher('button_go', Bool, queue_size=10)
-    msg2 = Bool(data=True)
-    pub2.publish(msg2)
+    pub = rospy.Publisher('button_open', Bool, queue_size=10)
+    msg = Bool(data=True)
+    pub.publish(msg)
     return render(request, 'end.html')
